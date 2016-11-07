ed3018d484b7c6b0fadd9f17b2e01d492f5bd17b
GreggLangley <gplang3@yahoo.com>
2016-11-07 06:16:25 +0000

    added momentjs

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 var express = require("express");
 var app = express();
+var moment = require("moment");
 
 app.use(express.static(__dirname + "/public"));
+
+app.get("/:input", function(request, resource){
+    var theDate;
+    if(parseFloat(request.params.input)){
+        theDate = moment(request.params.input, "X");
+    }else{
+        theDate = moment(request.params.input, "MMMM D, YYYY");
+    }
+    
+    if(theDate.isValid()){
+        resource.json({unix: theDate.format("X"), natural: theDate.format("MMMM D, YYYY")});
+    }else{
+        resource.json({unix: null, natural: null});
+    }
+});
 
 app.listen(8080, function(){
     console.log("example app listening on port 8080");
