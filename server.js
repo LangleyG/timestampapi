3efbea39eabf0fc97d7f0828d69ff89734068fbc
GreggLangley <gplang3@yahoo.com>
2016-11-07 03:51:45 +0000

    added pubilc folder and index.html

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var express = require("express");
 var app = express();
 
-app.get("/", function(request, resource){
-    resource.send("Hello World!");
-})
+app.use(express.static(__dirname + "/public"));
 
 app.listen(8080, function(){
     console.log("example app listening on port 8080");
