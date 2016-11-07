b4c0f4b40d0201e56b38e138f2c1071226c90a97
GreggLangley <gplang3@yahoo.com>
2016-11-07 07:15:49 +0000

    added procfile

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var moment = require("moment");
+var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + "/public"));
 
@@ -19,6 +20,6 @@ app.get("/:input", function(request, resource){
     }
 });
 
-app.listen(8080, function(){
-    console.log("example app listening on port 8080");
+app.listen(port, function(){
+    console.log("example app listening on port "+ port);
 })
